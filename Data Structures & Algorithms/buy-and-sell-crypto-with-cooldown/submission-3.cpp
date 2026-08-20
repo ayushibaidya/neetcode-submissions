bc5bf1ff84a7c074f2a4d32b60902bd0c7e77364
Ayushi Baidya <73297556+ayushibaidya@users.noreply.github.com>
2026-08-20 15:19:05 -0400

    Add: buy-and-sell-crypto-with-cooldown - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-3.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    int func(int index, int buy, vector<int>& prices,
+             vector<vector<int>>& dp) {
+
+        if(index >= prices.size())
+            return 0;
+
+        if(dp[index][buy] != -1)
+            return dp[index][buy];
+
+        if(buy == 1) {
+
+            return dp[index][buy] = max(
+                -prices[index] + func(index + 1, 0, prices, dp),
+                func(index + 1, 1, prices, dp)
+            );
+        }
+
+        return dp[index][buy] = max(
+            prices[index] + func(index + 2, 1, prices, dp),
+            func(index + 1, 0, prices, dp)
+        );
+    }
+
+    int maxProfit(vector<int>& prices) {
+
+        int n = prices.size();
+
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+
+        return func(0, 1, prices, dp);
+    }
+};
