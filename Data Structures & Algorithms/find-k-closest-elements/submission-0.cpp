d96faf94f0de8fb46791434803c31aa31e7f9236
Ayushi Baidya <73297556+ayushibaidya@users.noreply.github.com>
2026-09-23 13:28:41 -0400

    Add: find-k-closest-elements - submission-0

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int, int>> pq; 
+        vector<int> ans; 
+
+        for(int num:arr){
+            int diff = abs(x-num); 
+            pq.push({diff, num}); 
+            if(pq.size() > k){
+                pq.pop(); 
+            }
+        }
+
+        while(!pq.empty()){
+            ans.push_back(pq.top().second); 
+            pq.pop(); 
+        }
+
+        sort(ans.begin(), ans.end()); 
+        
+        return ans; 
+    }
+};
